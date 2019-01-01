efc302056ad528b7729dfd63d5092d7ecb9b526e
James Au <jamesaubudi@gmail.com>
2019-01-01 18:30:36 -0500

    Updates

---
+++ precache-manifest.ce5c9e82736d7d91248bf8d43b8c70c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70dbb9a2f326ea089ca",
+    "url": "/redditnbareader/static/js/runtime~main.f70dbb9a.js"
+  },
+  {
+    "revision": "7450d004c2a0db755dea",
+    "url": "/redditnbareader/static/js/main.7450d004.chunk.js"
+  },
+  {
+    "revision": "11fc1eda2c4d485ff525",
+    "url": "/redditnbareader/static/js/1.11fc1eda.chunk.js"
+  },
+  {
+    "revision": "7450d004c2a0db755dea",
+    "url": "/redditnbareader/static/css/main.2d158774.chunk.css"
+  },
+  {
+    "revision": "456209b0c36390461a5086f70ae759eb",
+    "url": "/redditnbareader/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/redditnbareader/precache-manifest.7569f0cfddf0eb678fb3202e77b10804.js"
+  "/redditnbareader/precache-manifest.ce5c9e82736d7d91248bf8d43b8c70c5.js"
 );
 
 workbox.clientsClaim();
