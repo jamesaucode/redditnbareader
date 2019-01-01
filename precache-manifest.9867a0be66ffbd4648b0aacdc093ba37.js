00ec6a5ee20202d502e7569e714bf238bdd85061
James Au <jamesaubudi@gmail.com>
2019-01-01 04:57:50 -0500

    Updates

---
+++ precache-manifest.9867a0be66ffbd4648b0aacdc093ba37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c551e8bd483595bf4f",
+    "url": "/jamesaucode/redditnbareader/static/js/runtime~main.84c551e8.js"
+  },
+  {
+    "revision": "de8027dc93a0a87c75e4",
+    "url": "/jamesaucode/redditnbareader/static/js/main.de8027dc.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/jamesaucode/redditnbareader/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "de8027dc93a0a87c75e4",
+    "url": "/jamesaucode/redditnbareader/static/css/main.6319505a.chunk.css"
+  },
+  {
+    "revision": "b43cbe4517e59dc1916ce632fc4da66d",
+    "url": "/jamesaucode/redditnbareader/index.html"
+  }
+];
